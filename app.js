6c18e51201a8754ea8777a7a4592e0d3d2868e2a
brktorres1-prog <brktorres1@gmail.com>
2026-06-03 20:37:53 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,25 @@
-alert("APP CARREGOU");
+const API =
+"https://script.google.com/macros/s/AKfycbz1yi-nk_DCgTSgFdbtoca19tjM4n6J9sI31M1dwnlbsVuJMJg5IzN_s17aFzBG8SPj/exec";
 
-document.getElementById("clientes").innerText = "999";
+async function carregarDashboard() {
+
+  try {
+
+    const resposta = await fetch(API + "?acao=dashboard");
+
+    const dados = await resposta.json();
+
+    document.getElementById("clientes").innerText =
+      dados.clientes || 0;
+
+  } catch (erro) {
+
+    console.error("Erro:", erro);
+
+    document.getElementById("clientes").innerText = "Erro";
+
+  }
+
+}
+
+carregarDashboard();
