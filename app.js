b8dd3780c64c298db0eb7fad1ac01107e74164a3
brktorres1-prog <brktorres1@gmail.com>
2026-06-03 20:51:31 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,17 +1,28 @@
 const API =
 "https://script.google.com/macros/s/AKfycbz1yi-nk_DCgTSgFdbtoca19tjM4n6J9sI31M1dwnlbsVuJMJg5IzN_s17aFzBG8SPj/exec";
 
-async function carregarDashboard(){
+async function carregarDashboard() {
 
-const resposta =
-await fetch(API + "?acao=dashboard");
+  try {
 
-const dados =
-await resposta.json();
+    const resposta = await fetch(API + "?acao=dashboard");
+    const dados = await resposta.json();
 
-document.getElementById("clientes")
-.innerText =
-dados.clientes;
+    document.getElementById("clientes").innerText =
+      dados.clientes || 0;
+
+    document.getElementById("sinistros").innerText =
+      dados.sinistros || 0;
+
+    document.getElementById("transportadores").innerText =
+      dados.transportadores || 0;
+
+    document.getElementById("planos").innerText =
+      dados.planos || 0;
+
+  } catch (erro) {
+    console.error("Erro:", erro);
+  }
 
 }
 
