f86d30a2a3571957ee0267813115921de988c0e9
brktorres1-prog <brktorres1@gmail.com>
2026-06-03 19:40:20 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,17 +1,24 @@
 const API =
-"https://script.google.com/macros/s/SUA_URL/exec";
+"https://script.google.com/macros/s/AKfycbz1yi-nk_DCgTSgFdbtoca19tjM4n6J9sI31M1dwnlbsVuJMJg5IzN_s17aFzBG8SPj/exec";
 
-async function carregarDashboard(){
+async function carregarDashboard() {
 
-const resposta =
-await fetch(API + "?acao=dashboard");
+  try {
 
-const dados =
-await resposta.json();
+    const resposta = await fetch(API + "?acao=dashboard");
 
-document.getElementById("clientes")
-.innerText =
-dados.clientes;
+    const dados = await resposta.json();
+
+    document.getElementById("clientes").innerText =
+      dados.clientes || 0;
+
+  } catch (erro) {
+
+    console.error("Erro:", erro);
+
+    document.getElementById("clientes").innerText = "Erro";
+
+  }
 
 }
 
